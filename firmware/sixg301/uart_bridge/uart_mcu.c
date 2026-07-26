7b79666f397b9bcc558f91aec9d2a7198b807fe6
Rossano Pantaleoni <rossano.pantaleoni@silabs.com>
2026-07-26 11:34:11 +0200

    chore(uart tx): Clean up uart bridge code

--- firmware/sixg301/uart_bridge/uart_mcu.c
+++ firmware/sixg301/uart_bridge/uart_mcu.c
@@ -5,13 +5,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "cmsis_os2.h"
-//#include "portmacrocommon.h"
 #include "sl_iostream.h"
 #include "sl_iostream_handles.h"
 #include "app_log.h"
 #include "app_assert.h"
 #include "sl_status.h"
-//#include "portable.h"
 #include <string.h>
 
 /*
@@ -21,7 +19,6 @@ uart_tx_message_t msg;                      // TX exchange message variable
 
 static osMessageQueueId_t uart_tx_queue;    // BLE -> MCU Tx Queue
 static osThreadId_t uart_tx_thread;         // MCU TX Task 
-//static TaskHandle_t uart_tx_task_handle = NULL;
 
 /*
  *  Function prototypes
@@ -46,7 +43,6 @@ void mcu_uart_init(void)
     };
 
     (void)MCU_UART_TX_BUFFER_SIZE;
-    //BaseType_t ret;
     // Create the MCU UART Tx queue
     uart_tx_queue = osMessageQueueNew(8U, sizeof(uart_tx_message_t),&queue_attributes);
     app_assert(uart_tx_queue != NULL, "Failed to create MCU UART TX queue\r\n");
@@ -54,19 +50,6 @@ void mcu_uart_init(void)
     // Create the MCU UART Tx Task
     uart_tx_thread = osThreadNew(mcu_uart_tx_task, NULL, &thread_attributes);
     app_assert(uart_tx_thread != NULL, "Failed to create MCU UART TX task\r\n");
-    // ret = xTaskCreate(
-    //     mcu_uart_tx_task,
-    //     "uart Tx task", 
-    //     512, 
-    //     NULL, 
-    //     tskIDLE_PRIORITY + 2, 
-    //     &uart_tx_task_handle);
-    // //app_assert(ret != 0, "Failed to create MCU UART TX task\r\n");
-    // if(ret != pdPASS) {
-    //     app_log("UART Task creation failed, free heap: %lu\r\n", (unsigned long)xPortGetFreeHeapSize());
-    // } else {
-    //     app_log("UART Task creation done, free heap: %lu\r\n", (unsigned long)xPortGetFreeHeapSize());
-    // }
 }
 
 /*
